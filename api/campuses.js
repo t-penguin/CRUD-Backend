61146288e4b5937075b4d8c43d04af6e829669a4
Joseph <sourpina@gmail.com>
2025-07-01 15:21:32 -0400

    Added empty route handlers for campus routes

---
+++ api/campuses.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const { Campus } = require("../database");
+
+// GET all campuses
+router.get("/", async (req, res) => {
+
+});
+
+// GET campus by ID
+router.get("/:id", async (req, res) => {
+
+});
+
+// POST new campuses
+router.get("/", async (req, res) => {
+
+});
+
+// DELETE campus by ID
+router.get("/:id", async (req, res) => {
+
+});
+
+module.exports = router;
--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const studentsRouter = require("./students");
+const campusesRouter = require("./campuses");
 
 router.use("/students", studentsRouter);
+router.use("/campuses", campusesRouter);
 
 module.exports = router;
