6dbb0ffa46dd64114fd4e1d63647235bd4ee5b83
Joseph <sourpina@gmail.com>
2025-07-01 15:29:45 -0400

    Added appropriate logic for the campuses routes
    Resolves #4 #5 #7 #9

--- api/campuses.js
+++ api/campuses.js
@@ -4,22 +4,52 @@ const { Campus } = require("../database");
 
 // GET all campuses
 router.get("/", async (req, res) => {
-
+  try {
+    const campuses = await Campus.findAll();
+    res.send(campuses);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
 });
 
 // GET campus by ID
 router.get("/:id", async (req, res) => {
-
+  try {
+    const campusID = Number(req.params.id);
+    const campus = await Campus.findByPk(campusID);
+    res.send(campus);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
 });
 
 // POST new campuses
 router.get("/", async (req, res) => {
-
+  try {
+    const campus = req.body;
+    await Campus.create(campus);
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
 });
 
 // DELETE campus by ID
 router.get("/:id", async (req, res) => {
-
+  try {
+    const campusID = Number(req.params.id);
+    const campus = await Campus.findByPk(campusID);
+    await campus.destroy();
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
 });
 
 module.exports = router;
