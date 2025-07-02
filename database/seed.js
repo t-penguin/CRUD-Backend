4f2b52844972324ad766a8dd71b20b0c16a5abcd
Joseph <sourpina@gmail.com>
2025-07-02 11:30:09 -0400

    Updated database seed with students and campuses

--- database/seed.js
+++ database/seed.js
@@ -1,17 +1,53 @@
 const db = require("./db");
-const { student } = require("./index");
+const { Student, Campus } = require("./index");
 
 const seed = async () => {
   db.logging = false;
   await db.sync({ force: true }); // Drop and recreate tables
-  const students = await Duck.bulkCreate([
-    { name: "James Pond" },
-    { name: "Quakie Chan" },
-    { name: "Goose" },
+
+  const campuses = await Campus.bulkCreate([
+    { 
+      name: "Borough of Manhattan Community College", 
+      description: "Borough of Manhattan Community College (BMCC) is a " +
+      "large community college in New York City, part of the City University " +
+      "of New York (CUNY) system. It's known for its diverse student body, " +
+      "offering over 50 associate degree programs and a wide range of continuing " +
+      "education options. BMCC is a leader in awarding degrees to minority " +
+      "students and is recognized for its role in increasing household income.",
+    },
   ]);
 
-  console.log(`🦆 Created ${ducks.length} ducks`);
+  const students = await Student.bulkCreate([
+    { 
+      firstName: "Joseph", 
+      lastName: "Collado",
+      email: "joseph@example.com",
+      gpa: 4.0,
+    },
+    { 
+      firstName: "Phone", 
+      lastName: "Swa",
+      email: "phone@example.com",
+      gpa: 4.0,
+    },
+    { 
+      firstName: "Olivia", 
+      lastName: "Wilson-Simmonds",
+      email: "olivia@example.com",
+      gpa: 4.0,
+      campusId: 1,
+    },
+    { 
+      firstName: "Muhammad", 
+      lastName: "Shaikh",
+      email: "muhammad@example.com",
+      gpa: 4.0,
+      campusId: 1,
+    },
+  ]);
 
+  console.log(`👨‍🎓👩‍🎓 Created ${students.length} students`);
+  console.log(`🏫 Created ${campuses.length} campuses`);
   console.log("🌱 Seeded the database");
   db.close();
 };
